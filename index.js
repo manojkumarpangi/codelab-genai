83f25744bbdcb7a8dee3cb8df06af92780765a63
manojkumarpangi <manojpangi@gmail.com>
2024-07-30 19:16:11 +0000

    minor changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
     });
 
     const animal = req.query.animal || 'dog';
-    const prompt = `Give me 10 fun facts aboutgit  ${animal}. Return this as html without backticks.`
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
     const resp = await generativeModel.generateContent(prompt);
     const html = resp.response.candidates[0].content.parts[0].text;
     res.send(html);
